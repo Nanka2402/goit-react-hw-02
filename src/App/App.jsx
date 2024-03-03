1cad940131c5acedbc4d9af1f0d4acc3fee8f14f
Nanka2402 <nastia.secr@gmail.com>
2024-03-03 23:32:07 +0200

    +

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,24 @@
+import Description from "../Description/Description";
 import "./App.css";
-// import Description from "./Description";
+import { useEffect, useState } from "react";
 // import Options from "./Options";
 // import Feedback from "./Feedback";
-// {
-// 	good: 0,
-// 	neutral: 0,
-// 	bad: 0
-// }
 
-const App = () => {
-  return <div>Sip Happens Café</div>;
-};
+export default function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const parsedFeedback = JSON.parse(localStorage.getItem'feedback');
+    return (
+      parsedFeedback || {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      }
+    );
+  });
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback))
+  }, [feedback]);
 
-export default App;
+  
+  return <Description />;
+}
--- src/Options/Options.jsx
+++ src/Options/Options.jsx
@@ -1,3 +1,49 @@
-// const updateFeedback = (feedbackType) => {
-//   // Тут використовуй сеттер, щоб оновити стан
-// };
+/* eslint-disable react/no-deprecated */
+/* eslint-disable no-unused-vars */
+import { PropTypes } from "react";
+import css from "./Options.module.css";
+
+export default function Options({ updateFeedback, total, resetFeedback }) {
+  return (
+    <div className={css.containerButton}>
+      <button
+        onClick={() => {
+          updateFeedback("good");
+        }}
+        type="button"
+        className={css.optionBtn}
+      >
+        Good
+      </button>
+      <button
+        onClick={() => {
+          updateFeedback("neutral");
+        }}
+        type="button"
+        className={css.optionBtn}
+      >
+        Neutral
+      </button>
+      <button
+        onClick={() => {
+          updateFeedback("bad");
+        }}
+        type="button"
+        className={css.optionBtn}
+      >
+        Bad
+      </button>
+      {total ? (
+        <button onClick={resetFeedback} type="button">
+          Reset
+        </button>
+      ) : null}
+    </div>
+  );
+}
+Options.PropTypes = {
+  handleClickGood: PropTypes.func.isRequired,
+  handleClickNeutral: PropTypes.func.isRequired,
+  handleClickBad: PropTypes.func.isRequired,
+  handleReset: PropTypes.func.isRequired,
+};
